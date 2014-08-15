73be9c78c6efd23e6cc76e66e2a67e002059b153
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-15 07:32:06 +0400

    progress of interactive map

--- inc/kontakty/imap.php
+++ inc/kontakty/imap.php
@@ -1,1 +1,13 @@
-<div class="imap" id="imap1"></div>
+<div class="imap" id="imap1">
+	<div class="map" data-coord-x="39.51593" data-coord-y="47.754769" data-zoom="14"></div>
+	<div class="marker">
+		<h3>Наш карьер:</h3>
+		<address>
+			<?$APPLICATION->IncludeFile(
+				'/inc/address.php',
+				array(),
+				array('SHOW_BORDER' => false)
+			);?>
+		</address>
+	</div>
+</div>
