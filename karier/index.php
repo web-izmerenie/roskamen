e00ec0531da2561507380e38000f289c81a5c1f5
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-15 10:54:52 +0400

    progress

--- karier/index.php
+++ karier/index.php
@@ -9,14 +9,32 @@ $APPLICATION->SetTitle("Карьер");
 		и&nbsp;поставщиком щебня в&nbsp;Ростове-на-Дону и&nbsp;области</p>
 	</blockquote>
 
-	<div class="video_block">
-		<a class="play"><span>Воспроизвести</span></a>
-		<video poster="/upload/karier/video_poster.jpg">
-			<source src="/upload/main_page/movie.mp4" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'>
-			<source src="/upload/main_page/movie.webm" type='video/webm; codecs="vp8.0, vorbis"'>
-			<source src="/upload/main_page/movie.ogg" type='video/ogg; codecs="theora, vorbis"'>
-			<p>Обновите ваш браузер чтобы просмотреть видео.</p>
-		</video>
+	<div class="info_block">
+		<figure>
+			<div class="video_block">
+				<a class="play"><span>Воспроизвести</span></a>
+				<video poster="/upload/karier/video_poster.jpg">
+					<source src="/upload/main_page/movie.mp4" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'>
+					<source src="/upload/main_page/movie.webm" type='video/webm; codecs="vp8.0, vorbis"'>
+					<source src="/upload/main_page/movie.ogg" type='video/ogg; codecs="theora, vorbis"'>
+					<p>Обновите ваш браузер чтобы просмотреть видео.</p>
+				</video>
+			</div>
+			<figcaption>
+				На&nbsp;сегодняшний день карьер, который мы&nbsp;разрабатываем с&nbsp;2013&nbsp;года, является одним<br>
+				из&nbsp;самых молодых и&nbsp;перспективных источников по&nbsp;средней выработке горной руды
+			</figcaption>
+		</figure>
+	</div>
+
+	<div class="info_block">
+		<figure>
+			<img alt="" src="/upload/karier/picture.jpg" />
+			<figcaption>
+				На&nbsp;нашем карьере мы&nbsp;используем только специализированную карьерную технику<br>
+				от&nbsp;лучших производителей Komatsu и&nbsp;Volvo
+			</figcaption>
+		</figure>
 	</div>
 </section>
 
