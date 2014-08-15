e4456bface69d8ac44e7385b3ceafdac0f6eab16
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-08-15 06:52:41 +0400

    imap resize

--- bitrix/templates/general/scripts/src/pages/kontakty.js
+++ bitrix/templates/general/scripts/src/pages/kontakty.js
@@ -4,17 +4,30 @@
  * @author Viacheslav Lotsmanov
  */
 
-define(['jquery', 'styles_ready'], function ($, stylesReady) {
+define(['jquery', 'styles_ready', 'get_val'], function ($, stylesReady, getVal) {
 $(function domReady() {
 var $s = $('section.contacts');
 if ($s.size() <= 0) return;
+
+var imapResizeBindSuffix = '.interactive_map_resize';
+var $w = $(window);
+var imapRatio = getVal('kontaktyMapRatio');
+
 stylesReady(function () {
 
 	$s.each(function () {
 		var $s = $(this);
-		var $imap = $s.find('.imap');
+		var $imap = $s.find('.imap'); // interactive map
 
-		//
+		$w.on(
+			'resize' + imapResizeBindSuffix,
+			$.proxy(setTimeout, null, function () {
+				$imap.css(
+					'height',
+					$imap.width() * imapRatio[1] / imapRatio[0] + 'px'
+				);
+			}, 1)
+		).trigger('resize' + imapResizeBindSuffix);
 	});
 
 }); // stylesReady()
