f40a6cd3c71d35cc28b7cd988870eb8225bcafad
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-09-17 04:56:26 +0400

    apply "karier" section to bitrix (advantages as iblock)

---
+++ inc/karier/karier.php
@@ -0,0 +1,139 @@
+<section class="pit">
+	<blockquote class="yellow">
+		<p>Компания ООО &laquo;Роскамень&raquo; является крупным производителем<br>
+		и&nbsp;поставщиком щебня в&nbsp;Ростове-на-Дону и&nbsp;области</p>
+	</blockquote>
+
+	<div class="info_block">
+		<figure>
+			<div class="video_block">
+				<a class="play"><span>Воспроизвести</span></a>
+				<video poster="/upload/karier/video_poster.jpg">
+					<source src="/upload/karier/video/video.mp4" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'>
+					<source src="/upload/karier/video/video.webm" type='video/webm; codecs="vp8.0, vorbis"'>
+					<source src="/upload/karier/video/video.ogg" type='video/ogg; codecs="theora, vorbis"'>
+					<p>Обновите ваш браузер чтобы просмотреть видео.</p>
+				</video>
+			</div>
+			<figcaption>
+				На&nbsp;сегодняшний день карьер, который мы&nbsp;разрабатываем с&nbsp;2013&nbsp;года, является одним<br>
+				из&nbsp;самых молодых и&nbsp;перспективных источников по&nbsp;средней выработке горной руды
+			</figcaption>
+		</figure>
+	</div>
+
+	<div class="info_block">
+		<figure>
+			<img alt="" src="/upload/karier/picture.jpg">
+			<figcaption>
+				На&nbsp;нашем карьере мы&nbsp;используем только специализированную карьерную технику<br>
+				от&nbsp;лучших производителей Komatsu и&nbsp;Volvo
+			</figcaption>
+		</figure>
+	</div>
+
+	<h2>С нами работать выгодно</h2>
+
+	<?$APPLICATION->IncludeComponent(
+		"bitrix:news.list", 
+		"advantages", 
+		array(
+			"IBLOCK_TYPE" => "service",
+			"IBLOCK_ID" => "1",
+			"NEWS_COUNT" => "50",
+			"SORT_BY1" => "SORT",
+			"SORT_ORDER1" => "ASC",
+			"SORT_BY2" => "",
+			"SORT_ORDER2" => "",
+			"FILTER_NAME" => "",
+			"FIELD_CODE" => array(
+				0 => "NAME",
+				1 => "SORT",
+				2 => "PREVIEW_PICTURE",
+				3 => "",
+			),
+			"PROPERTY_CODE" => array(
+				0 => "",
+				1 => "",
+			),
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "N",
+			"AJAX_OPTION_HISTORY" => "N",
+			"CACHE_TYPE" => "A",
+			"CACHE_TIME" => "36000000",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"ACTIVE_DATE_FORMAT" => "",
+			"SET_TITLE" => "N",
+			"SET_BROWSER_TITLE" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_STATUS_404" => "N",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"DISPLAY_DATE" => "N",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "N",
+			"PAGER_TEMPLATE" => "",
+			"DISPLAY_TOP_PAGER" => "N",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"PAGER_TITLE" => "",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "",
+			"PAGER_SHOW_ALL" => "N",
+			"AJAX_OPTION_ADDITIONAL" => ""
+		),
+		false
+	);?>
+	<!--<ul class="advantages">
+		<li>
+			<img alt="" src="/upload/karier/circles/way.png">
+			<p>Карьер расположен на 68 км<br>от Ростова-на-Дону</p>
+		</li>
+		<li>
+			<img alt="" src="/upload/karier/circles/time.png">
+			<p>Отгрузка щебня — 24 часа 7 дней<br> в неделю</p>
+		</li>
+		<li>
+			<img alt="" src="/upload/karier/circles/pyramid.png">
+			<p>Лещадность 8–10%</p>
+		</li>
+		<li>
+			<img alt="" src="/upload/karier/circles/car.png">
+			<p>Парк разногрузной техники<br>и возможность самовывоза</p>
+		</li>
+		<li>
+			<img alt="" src="/upload/karier/circles/weight.png">
+			<p>Отгрузим точно<br>от 10 до 100 тонн продукции</p>
+		</li>
+		<li>
+			<img alt="" src="/upload/karier/circles/percent.png">
+			<p>Гибкая система скидок<br>и доступные цены</p>
+		</li>
+	</ul>-->
+
+	<div class="info_block specific">
+		<figure>
+			<img alt="" src="/upload/karier/stones.jpg">
+			<figcaption>
+				<p>Высокое качество нашей продукции подтверждается <a href="#">сертификатами</a> и&nbsp;<a href="#">паспортами</a>.</p>
+				<p>Щебень, выработанный в&nbsp;нашем карьере, был допущен в&nbsp;составе основных поставок строительных материалов при возведении военного аэродрома в&nbsp;Крымске.</p>
+			</figcaption>
+		</figure>
+	</div>
+
+	<blockquote class="green">
+		<p>Узнать о&nbsp;стоимости продукции и&nbsp;заказать щебень вы&nbsp;можете,<br>
+		позвонив по&nbsp;круглосуточному телефону: +7&nbsp;903 489-21-59</p>
+	</blockquote>
+</section>
--- karier/index.php
+++ karier/index.php
@@ -3,83 +3,10 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Карьер");
 ?>
 
-<section class="pit">
-	<blockquote class="yellow">
-		<p>Компания ООО &laquo;Роскамень&raquo; является крупным производителем<br>
-		и&nbsp;поставщиком щебня в&nbsp;Ростове-на-Дону и&nbsp;области</p>
-	</blockquote>
-
-	<div class="info_block">
-		<figure>
-			<div class="video_block">
-				<a class="play"><span>Воспроизвести</span></a>
-				<video poster="/upload/karier/video_poster.jpg">
-					<source src="/upload/karier/video/video.mp4" type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'>
-					<source src="/upload/karier/video/video.webm" type='video/webm; codecs="vp8.0, vorbis"'>
-					<source src="/upload/karier/video/video.ogg" type='video/ogg; codecs="theora, vorbis"'>
-					<p>Обновите ваш браузер чтобы просмотреть видео.</p>
-				</video>
-			</div>
-			<figcaption>
-				На&nbsp;сегодняшний день карьер, который мы&nbsp;разрабатываем с&nbsp;2013&nbsp;года, является одним<br>
-				из&nbsp;самых молодых и&nbsp;перспективных источников по&nbsp;средней выработке горной руды
-			</figcaption>
-		</figure>
-	</div>
-
-	<div class="info_block">
-		<figure>
-			<img alt="" src="/upload/karier/picture.jpg">
-			<figcaption>
-				На&nbsp;нашем карьере мы&nbsp;используем только специализированную карьерную технику<br>
-				от&nbsp;лучших производителей Komatsu и&nbsp;Volvo
-			</figcaption>
-		</figure>
-	</div>
-
-	<h2>С нами работать выгодно</h2>
-
-	<ul class="advantages">
-		<li>
-			<img alt="" src="/upload/karier/circles/way.png">
-			<p>Карьер расположен на 68 км<br>от Ростова-на-Дону</p>
-		</li>
-		<li>
-			<img alt="" src="/upload/karier/circles/time.png">
-			<p>Отгрузка щебня — 24 часа 7 дней<br> в неделю</p>
-		</li>
-		<li>
-			<img alt="" src="/upload/karier/circles/pyramid.png">
-			<p>Лещадность 8–10%</p>
-		</li>
-		<li>
-			<img alt="" src="/upload/karier/circles/car.png">
-			<p>Парк разногрузной техники<br>и возможность самовывоза</p>
-		</li>
-		<li>
-			<img alt="" src="/upload/karier/circles/weight.png">
-			<p>Отгрузим точно<br>от 10 до 100 тонн продукции</p>
-		</li>
-		<li>
-			<img alt="" src="/upload/karier/circles/percent.png">
-			<p>Гибкая система скидок<br>и доступные цены</p>
-		</li>
-	</ul>
-
-	<div class="info_block specific">
-		<figure>
-			<img alt="" src="/upload/karier/stones.jpg">
-			<figcaption>
-				<p>Высокое качество нашей продукции подтверждается <a href="#">сертификатами</a> и&nbsp;<a href="#">паспортами</a>.</p>
-				<p>Щебень, выработанный в&nbsp;нашем карьере, был допущен в&nbsp;составе основных поставок строительных материалов при возведении военного аэродрома в&nbsp;Крымске.</p>
-			</figcaption>
-		</figure>
-	</div>
-
-	<blockquote class="green">
-		<p>Узнать о&nbsp;стоимости продукции и&nbsp;заказать щебень вы&nbsp;можете,<br>
-		позвонив по&nbsp;круглосуточному телефону: +7&nbsp;903 489-21-59</p>
-	</blockquote>
-</section>
+<?$APPLICATION->IncludeFile(
+	'/inc/karier/karier.php',
+	array(),
+	array('SHOW_BORDER' => false)
+);?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
