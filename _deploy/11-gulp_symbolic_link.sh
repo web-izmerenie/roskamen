da5eeeb37d32fda32799221aa64e104b4af7030e
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-10-21 11:59:14 +0400

    deploy by "web-front-end-deploy" git repo as submodule

--- _deploy/11-gulp_symbolic_link.sh
+++ _deploy/11-gulp_symbolic_link.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")" WD="$(dirname "$0")/../" ../deploy.sh; exit "$?"; fi
-
-info_inline "Creating symbolic link to gulp-cli"
-rm ./gulp &>/dev/null
-if ln -s ./node_modules/.bin/gulp ./gulp &>/dev/null; then ok; else err; fi
+../web-front-end-deploy/tasks/11-gulp_symbolic_link.sh
--- _deploy/13-gulp_default_tasks.sh
+++ _deploy/13-gulp_default_tasks.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")" WD="$(dirname "$0")/../" ../deploy.sh; exit "$?"; fi
-
-info "Starting default gulp tasks"
-if ./gulp; then
-	info_inline "Default gulp tasks status:"; ok;
-else err; fi
+../web-front-end-deploy/tasks/13-gulp_default_tasks.sh
